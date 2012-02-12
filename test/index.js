a5699c6b0cc960aed827b3cec569417052e531a9
Joe LeBlanc <contact@jlleblanc.com>
2012-02-11 21:11:13 -0600

    Added a test suite with a sample configuration.php file

---
+++ test/index.js
@@ -0,0 +1,34 @@
+var joomla = require('../lib/joomla');
+
+describe('Joomla', function  () {
+  describe('constructor', function  () {
+    it('should take blank object', function (done) {
+      joomla({
+        db: 'test_joomla_node',
+        user: 'root',
+        host: 'localhost',
+        password: ''
+      }, done);
+    });
+  });
+  
+  describe('constructor', function  () {
+    it('should take path to Joomla installation', function (done) {
+      joomla(__dirname, done);
+    });
+  });
+
+  describe('query', function  () {
+    it('should return a list of users', function  (done) {
+      var query = "SELECT * FROM #__users";
+
+      joomla.db.query(query, function  (results) {
+        if (results === undefined) {
+          throw "No results from " + query;
+        }
+        
+        done();
+      });
+    });
+  });
+});
